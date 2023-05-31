a8b375f9052059e15a507fdeefb7fc8ef3359905
Guido Percú <guidopercu@gmail.com>
2023-05-31 16:59:07 -0300

    Using rss-parser instead

--- src/handlers/scheduled-event-logger.mjs
+++ src/handlers/scheduled-event-logger.mjs
@@ -2,40 +2,35 @@
  * A Lambda function that logs the payload received from a CloudWatch scheduled event.
  */
 
-import { feedparser } from 'feedparser';
-import fetch from 'node-fetch';
+import Parser from 'rss-parser';
+
+async function readRssFeed(feedUrl) {
+  const parser = new Parser();
+
+  return await parser.parseURL(feedUrl);
+}
 
 export const scheduledEventLoggerHandler = async (event, context) => {
     console.info(JSON.stringify(event));
 
     const podcast_feed_url = "https://innerfrench.com/feed/"
-    var req = fetch(podcast_feed_url)
-    var feed = new FeedParser();
 
-    req.then(function (res) {
-        if (res.status !== 200) {
-          throw new Error('Error on retrieving the feed url');
-        }
-        else {
-          // The response `body` -- res.body -- is a stream
-          res.body.pipe(feed);
-        }
-      }, function (err) {
-        throw new Error('Error on downloading the feed');
-      });
+    let feed;
+    let feed_links = [];
+  
+    feed = await readRssFeed(podcast_feed_url);
+    if (!feed){
+      console.log('Failed to get RSS');
+      return [];
+    }
+  
+    feed.items.forEach((item) => {
+      console.log(item);
+      feed_titles.push(item.title);
+      feed_links.push(item.link);
+    });
 
-      feed.on('error', function (error) {
-        throw new Error('Error on feedparser');
-      });
+    console.log(feed_links);
+    return feed_links;
 
-      feed.on('readable', function () {
-        // This is where the action is!
-        var stream = this; // `this` is `feedparser`, which is a stream
-        var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-        var item;
-
-        while (item = stream.read()) {
-          console.log(item);
-        }
-      });
 }
